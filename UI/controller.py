7da848a8e4d3d14e9f1a01a9030329644a75e383
alexandraholota <s297022@studenti.polito.it>
2024-07-06 13:28:10 +0200

    Modifica controller

--- UI/controller.py
+++ UI/controller.py
@@ -12,53 +12,52 @@ class Controller:
         self._fonteRinnovabile = None
 
     def get_SoluzioneCliente(self, e):
+        importo = -1
+        capacita = -1
         self._view._txt_resultSoluzione.controls.clear()
-        if self._view._txtImporto.value != "":
+        if self._view._txtImporto.value != "" and self._view._txtCapacita.value != "" :
             try:
                 importo = float(self._view._txtImporto.value)
             except ValueError:
-                self._view.txt_resultSoluzione.controls.append(ft.Text("Investimento iniziale non valido"))
+                self._view._txt_resultSoluzione.controls.append(
+                    ft.Text("Investimento iniziale non valido, inserire un importo"))
                 self._view.update_page()
                 return
-        else:
-            self._view.txt_resultSoluzione.controls.append(ft.Text("Inserire l' investimento iniziale desiderato"))
-            self._view.update_page()
-            return
-        if self._view._txtCapacita != "":
             try:
                 capacita = float(self._view._txtCapacita.value)
             except ValueError:
-                self._view.txt_resultSoluzione.controls.append(ft.Text("Capacità non valida"))
+                self._view._txt_resultSoluzione.controls.append(
+                    ft.Text("Capacità non valida"))
                 self._view.update_page()
                 return
-        else:
-            self._view.txt_resultSoluzione.controls.append(ft.Text("Inserire la capacità desidera"))
+        elif self._view._txtImporto.value == "" or self._view._txtCapacita.value == ""  :
+            self._view._txt_resultSoluzione.controls.append(
+                ft.Text("Inserire i dati richiesti"))
             self._view.update_page()
             return
 
         if self._fonteRinnovabile == None:
-            self._view.txt_resultSoluzione.controls.append(ft.Text("Inserire la fonte rinnovabile"))
+            self._view._txt_resultSoluzione.controls.append(
+                ft.Text("Inserire la fonte rinnovabile"))
             self._view.update_page()
             return
 
         self.grafo = self._model.buildGrafo(self._fonteRinnovabile, importo, capacita)
-        # self._view._txt_resultSoluzione.controls.append(ft.Text("Grafo correttamente creato."))
-        n, a = self._model.getGraphDetails()
-        # self._view._txt_resultSoluzione.controls.append(ft.Text(
-        #    f"Il grafo è costituito di {n} nodi e {a} archi."))
 
         self._model.getBestNodes()
 
         if len(self._model.bestNodes) != 0:
-            self._view._txt_resultSoluzione.controls.append(ft.Text(f"Abbiamo trovato {len(self._model.bestNodes)} soluzioni ottime: "))
+            self._view._txt_resultSoluzione.controls.append(
+                ft.Text(f"Abbiamo trovato {len(self._model.bestNodes)} soluzioni ottime: "))
             for n in self._model.bestNodes:
                 self._view._txt_resultSoluzione.controls.append(ft.Text(
-                    f"{self._model._idMap[n].id} : Investimento inziale (USD): {self._model._idMap[n].Initial_Investment_USD} - "
-                    f"Capacità installata (MW) : {self._model._idMap[n].Installed_Capacity_MW} - "
-                    f"Indice di riduzione dell’inquinamento atmosferico: {self._model._idMap[n].Air_Pollution_Reduction_Index}  "))
+                    f"{self._model._idMap[n].id} -  "
+                    f"Investimento inziale (USD): {self._model._idMap[n].Initial_Investment_USD} \n "
+                    f"Capacità installata (MW) : {self._model._idMap[n].Installed_Capacity_MW} \n "
+                    f"Indice di riduzione dell’inquinamento atmosferico: "
+                    f"{self._model._idMap[n].Air_Pollution_Reduction_Index}  "))
         else:
             self._view._txt_resultSoluzione.controls.append(ft.Text(f"Non abbiamo trovato nessuna soluzione "))
-
 
         self._view.update_page()
 
@@ -70,30 +69,41 @@ class Controller:
                 minimo = self._model.analisiGeneraleMin(self._fonteRinnovabile)
                 massimo = self._model.analisiGeneraleMax(self._fonteRinnovabile)
                 media = self._model.analisiGeneraleAVG(self._fonteRinnovabile)
-                self._view._txt_resultAnalisi.controls.append(ft.Text("Analisi Generale: "))
-                self._view._txt_resultAnalisi.controls.append(ft.Text("I valori di minimo sono: "))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text("Analisi Generale: "))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text("I valori minimi sono: "))
                 self.stampaValoriAnalisi(minimo)
-                self._view._txt_resultAnalisi.controls.append(ft.Text("---------------------------"))
-                self._view._txt_resultAnalisi.controls.append(ft.Text("I valori di massimo sono: "))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text("---------------------------"))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text("I valori massimi sono: "))
                 self.stampaValoriAnalisi(massimo)
-                self._view._txt_resultAnalisi.controls.append(ft.Text("---------------------------"))
-                self._view._txt_resultAnalisi.controls.append(ft.Text("I valori di medi sono: "))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text("---------------------------"))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text("I valori medi sono: "))
                 self.stampaValoriAnalisi(media)
             else:
                 minimo = self._model.analisiSpecificaMin(self._fonteRinnovabile, self._analisi)
                 massimo = self._model.analisiSpecificaMax(self._fonteRinnovabile, self._analisi)
                 media = self._model.analisiSpecificaAVG(self._fonteRinnovabile, self._analisi)
-                self._view._txt_resultAnalisi.controls.append(ft.Text(f"Analisi specifica per la categoria {self._view._ddAttributes.value}: "))
-                self._view._txt_resultAnalisi.controls.append(ft.Text(f"Il valore minimo trovato è: {minimo}"))
-                self._view._txt_resultAnalisi.controls.append(ft.Text(f"Il valore massimo trovato è: {massimo}"))
-                self._view._txt_resultAnalisi.controls.append(ft.Text(f"Il valore medio calcoltao è: {media} "))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text(f"Analisi specifica per la categoria {self._view._ddAttributes.value}: "))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text(f"Il valore minimo trovato è: {minimo}"))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text(f"Il valore massimo trovato è: {massimo}"))
+                self._view._txt_resultAnalisi.controls.append(
+                    ft.Text(f"Il valore medio calcoltao è: {media} "))
 
             self._view._txtImporto.disabled = False
             self._view._txtCapacita.disabled = False
             self._view._btnSoluzione.disabled = False
             self._view.update_page()
         else:
-            self._view._txt_resultAnalisi.controls.append(ft.Text(f"Non è stata scelto un sistema energetico rinnovabile da analizzare"))
+            self._view._txt_resultAnalisi.controls.append(
+                ft.Text(f"Non è stata scelto un sistema energetico rinnovabile da analizzare"))
             self._view.update_page()
             return
 
@@ -101,20 +111,33 @@ class Controller:
     def populated_ddFonteRinnovabile(self):
         fonti = self._model.getAllRenewableSource()
         for f in fonti:
-            self._view._ddFonteRinnovabile.options.append(ft.dropdown.Option(data=f.id, on_click=self.readDDFonteRinnovabile, text=f.tipo))
+            self._view._ddFonteRinnovabile.options.append(
+                ft.dropdown.Option(
+                    data=f.id, on_click=self.readDDFonteRinnovabile,
+                    text=f.tipo))
         self._view.update_page()
 
     def populated_ddAttributes(self):
-        self._view._ddAttributes.options.append(ft.dropdown.Option(
-            data="Analisi Generale",
-            on_click=self.readDDAnalisi,
-            text="Analisi Generale"))
+        self._view._ddAttributes.options.append(
+            ft.dropdown.Option(
+                data="Analisi Generale",
+                on_click=self.readDDAnalisi,
+                text="Analisi Generale"))
         opzioni = self._model.getAllOptions()
         for o in opzioni:
-            self._view._ddAttributes.options.append(ft.dropdown.Option(
-                data=o.attributes, text=o.attributi, on_click=self.readDDAnalisi
+            self._view._ddAttributes.options.append(
+                ft.dropdown.Option(
+                    data=o.attributes,
+                    text=o.attributi,
+                    on_click=self.readDDAnalisi
             ))
         self._view.update_page()
+
+    def readDDFonteRinnovabile(self, e):
+        if e.control.data is None:
+            self._fonteRinnovabile = None
+        else:
+            self._fonteRinnovabile = e.control.data
 
     def readDDAnalisi(self, e):
         if e.control.data is None:
@@ -123,17 +146,12 @@ class Controller:
             self._analisi = e.control.data
 
 
-    def readDDFonteRinnovabile(self, e):
-        if e.control.data is None:
-            self._fonteRinnovabile = None
-        else:
-            self._fonteRinnovabile = e.control.data
-
     def stampaValoriAnalisi(self, valori):
         for k in valori.keys():
             self._view._txt_resultAnalisi.controls.append(
                 ft.Text(f"{k}: {valori[k]}")
             )
+
 
 
 
